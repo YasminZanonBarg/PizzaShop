2806afac74e4a905d05f511a52ae14c837e1e90e
yasmin.barg <yasmin.barg@hotmail.com>
2025-03-17 16:43:21 -0300

    toast notification (sonner)

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,4 +1,5 @@
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
@@ -20,9 +21,21 @@ export function SignIn() {
   } = useForm<SignInForm>()
 
   async function handleSignIn(data: SignInForm) {
-    console.log(data)
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000))
 
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+      toast.success('Enviamos um link de autenticação para seu e-mail.', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => {
+            handleSignIn(data)
+          },
+        },
+      })
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+      toast.error('Credenciais inválidas.')
+    }
   }
 
   return (
